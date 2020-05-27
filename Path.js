a74a9bb08e1e16b0fff89fd63dc75b5e3a8134d7
Keszericze István <Keszericze@richter.hu>
2020-05-27 12:51:07 +0200

    Beginning

---
+++ Path.js
@@ -0,0 +1,9 @@
+console.log("Start")
+// D3 offers generators to create shapes
+// line generator
+var dataArray=[{x:5,y:5},{x:10,y:15},{x:20,y:7},{x:30,y:18},{x:40,y:10}];
+
+var svg=d3.select('body').append('svg')
+    .attr('height','100%')
+    .attr('width','100%');
+
